735e4cfdf4eb7f9fe4951b928838f145325e7646
Nguyen Duy Anh <166126284+hirentg@users.noreply.github.com>
2024-11-26 17:58:09 +0700

    Add files via upload

---
+++ Lab03/AimsProject/src/hust/soict/dsai/aims/store/Store.java
@@ -0,0 +1,44 @@
+package hust.soict.dsai.aims.store;
+
+import hust.soict.dsai.disc.DigitalVideoDisc;
+
+public class Store {
+    private static final int MAX_ITEMS = 100;
+    private DigitalVideoDisc[] itemsInStore = new DigitalVideoDisc[MAX_ITEMS];
+    private int itemCount = 0;
+
+    public void addDVD(DigitalVideoDisc disc) {
+        if (itemCount < MAX_ITEMS) {
+            itemsInStore[itemCount] = disc;
+            itemCount++;
+            System.out.println("DVD added to store successfully");
+        } else {
+            System.out.println("Store is full");
+        }
+    }
+
+    public void removeDVD(DigitalVideoDisc disc) {
+        for (int i = 0; i < itemCount; i++) {
+            if (itemsInStore[i] == disc) {
+                // Shift elements to remove the DVD
+                for (int j = i; j < itemCount - 1; j++) {
+                    itemsInStore[j] = itemsInStore[j + 1];
+                }
+                itemsInStore[itemCount - 1] = null;
+                itemCount--;
+                System.out.println("DVD removed from store successfully");
+                return;
+            }
+        }
+        System.out.println("DVD not found in store");
+    }
+
+    public void print() {
+        System.out.println("***********************STORE***********************");
+        System.out.println("Items in store:");
+        for (int i = 0; i < itemCount; i++) {
+            System.out.printf("%d. %s\n", i + 1, itemsInStore[i].toString());
+        }
+        System.out.println("***********************************************");
+    }
+}
---
+++ Lab03/AimsProject/src/hust/soict/dsai/test/store/StoreTest.java
@@ -0,0 +1,38 @@
+
+package hust.soict.dsai.test.store;
+
+import hust.soict.dsai.aims.store.Store;
+import hust.soict.dsai.disc.DigitalVideoDisc;
+
+public class StoreTest {
+    public static void main(String[] args) {
+        // Create sample DVDs
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 124, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", "Ron Clements", 90, 18.99f);
+
+        // Create a store and add DVDs
+        Store store = new Store();
+        store.addDVD(dvd1);
+        store.addDVD(dvd2);
+
+        // Print store contents
+        System.out.println("Store contents after adding DVDs:");
+        store.print();
+
+        // Test removing a DVD
+        System.out.println("\nRemoving 'Star Wars' from store:");
+        store.removeDVD(dvd2);
+        store.print();
+
+        // Attempt to remove a DVD not in store
+        System.out.println("\nAttempting to remove 'Aladdin' from store:");
+        store.removeDVD(dvd3);
+        store.print();
+
+        // Add another DVD
+        System.out.println("\nAdding 'Aladdin' to store:");
+        store.addDVD(dvd3);
+        store.print();
+    }
+}
